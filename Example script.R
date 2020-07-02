767cea77972d765626f642e4e5a2d072a34988e0
yiwenw <yvonne515wang@gmail.com>
2020-07-02 18:45:08 -0500

    add more informaiton

--- Example script.R
+++ Example script.R
@@ -8,7 +8,8 @@ write.csv(x = CO2, file = "datafile.csv")
 
 ## to ignore folder in git -> gitignore, type file names or folder names (ex: Inputs/ Outputs/)
 
-## saving on the local repository
-## commit the change and push it to the github (control A to select all files, and hit stage to add all the files at the same time)
+## saving on the working directionary
+## commit the change to local repository (control A to select all files, and hit stage to add all the files at the same time)
    ## add commit message to keep tracking different version (ex: adding or deleting the code; more detail the commit message, the better)
+## push the change to the github by upload bottom
 ## from github, pull the data to local repository
