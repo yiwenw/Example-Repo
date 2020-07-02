d0541635bc02bb6de002c158eadfd8b503808996
yiwenw <yvonne515wang@gmail.com>
2020-07-02 18:56:06 -0500

    update in github

--- Example script.R
+++ Example script.R
@@ -12,4 +12,10 @@ write.csv(x = CO2, file = "datafile.csv")
 ## commit the change to local repository (control A to select all files, and hit stage to add all the files at the same time)
    ## add commit message to keep tracking different version (ex: adding or deleting the code; more detail the commit message, the better)
 ## push the change to the github by upload bottom
-## from github, pull the data to local repository
+## from github, pull the data to working directionary
+
+## one can also revert the change back by hitting commit and select revert
+
+## pulling changes down: only happen when cooperation in same documents
+   ## one can edited on github, and commit change
+   ## in Rstudio, pull the changes into local copy of R script
