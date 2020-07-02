9c966c63da84d2657578b03d1d74a9c2c75bcc06
yiwenw <yvonne515wang@gmail.com>
2020-07-02 18:32:28 -0500

    + initial commit

---
+++ Example script.R
@@ -0,0 +1,14 @@
+data(CO2)
+write.csv(x = CO2, file = "datafile.csv")
+
+
+## git is a good place to store code, but terrible place to store data
+## do not store data more than 100mb
+
+
+## to ignore folder in git -> gitignore, type file names or folder names (ex: Inputs/ Outputs/)
+
+## saving on the local repository
+## commit the change and push it to the github (control A to select all files, and hit stage to add all the files at the same time)
+   ## add commit message to keep tracking different version (ex: adding or deleting the code; more detail the commit message, the better)
+## from github, pull the data to local repository
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Example-Repo
+
+Enter some text from Rstudio
+## git is a good place to store code, but terrible place to store data
+## do not store data more than 100mb
+
+
+## In R script, to ignore folder in git -> gitignore, type file names or folder names (ex: Inputs/ Outputs/)
